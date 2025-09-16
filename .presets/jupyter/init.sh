e95a48af9fd1f697d1919442fbc03deabb1b8604
Kobus van der Merwe <kobusvdm@gmail.com>
2025-09-16 21:42:00 +0200

    Add notebook-intelligence

--- .presets/jupyter/init.sh
+++ .presets/jupyter/init.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-poetry add jupyterlab jupyterlab-lsp jupyter-lsp python-lsp-server
+poetry add jupyterlab jupyterlab-lsp jupyter-lsp python-lsp-server jupyter-bokeh ipywidgets-bokeh notebook-intelligence
 poetry install --no-root
 
